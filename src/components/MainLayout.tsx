c1decafb1679e9cf10fb5ff440e149f3bc4f731e
dapin metoti <davinmetotii@gmail.com>
2026-06-22 21:50:01 +0700

    feat: add MainLayout component with Outlet and static BottomNav

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout() {
   return (
     <div className="w-full h-full min-h-screen flex flex-col bg-brand-primary-dark relative">
       {/* Scrollable page content */}
-      <div className="flex-1 w-full pb-16">
+      <div className="flex-1 w-full pb-16 ">
         <Outlet />
       </div>
       {/* Static bottom navbar that doesn't animate */}
